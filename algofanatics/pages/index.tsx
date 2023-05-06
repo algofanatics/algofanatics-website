8b420008eb584eec4280bf96e5e9d4eb5ccdd6ca
jessica <jessicajoseph2019@yahoo.com>
2023-05-06 11:33:51 +0100

    feat:project set up

---
+++ algofanatics/pages/index.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar/Navbar";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Header />
+      <Footer />
+    </main>
+  );
+}
