8d9e7fe7e42ebee29f481b75c206c969f028bc4a
jessica <jessicajoseph2019@yahoo.com>
2023-05-11 19:22:11 +0100

    Form UI

--- algofanatics/components/Form/Form.tsx
+++ algofanatics/components/Form/Form.tsx
@@ -5,9 +5,9 @@ const Form = () => {
     <main className="pt-20 lg:pt-28">
       <div className="bg-black container mx-auto items-center lg:px-20 py-10 flex-col flex lg:flex-row justify-between">
         <h1 className="text-primary text-2xl pb-8 font-bold lg:text-4xl font-work">Request Our Services</h1>
-        <form className="bg-white md:w-[380px] px-5 lg:-mt-16 rounded-sm">
+        <div className="bg-white md:w-[380px] px-5 lg:-mt-16 rounded-sm">
           <FormComponent />
-        </form>
+        </div>
       </div>
     </main>
   );
