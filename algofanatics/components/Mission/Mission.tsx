57a088bee505e13f5fab899cdc86452fa0f8b283
jessica <jessicajoseph2019@yahoo.com>
2023-05-11 15:21:49 +0100

    feat: about, team, mission and header UI

--- algofanatics/components/Mission/Mission.tsx
+++ algofanatics/components/Mission/Mission.tsx
@@ -14,7 +14,7 @@ const Mission = () => {
           />
         </div>
         
-        <div className="lg:pl-16 text-justify lg:max-w-xl font-normal w-full">
+        <div className="lg:pl-20 text-justify lg:max-w-xl font-normal w-full">
           <h1 className="text-3xl lg:text-4xl font-work font-semibold">Our Mission</h1>
           <p className="py-3 lg:text-lg font-nunito text-sm md:text-lg leading-10">
             Our mission at Algofanatics is to empower struggling developers by
