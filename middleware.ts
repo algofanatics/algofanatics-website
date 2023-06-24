1bf397fb0f4e7b7e616626283df217641061b02d
jessica <jessicajoseph2019@yahoo.com>
2023-06-24 15:16:48 +0100

    feat: blog API integration & protected route with middleware

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-//   const allowedPaths = [
-//     "/",
-//     "/forgotpassword",
-//     "/forgotpassword/otp",
-//     "/success",
-//   ];
-
   const requestedPath = request.nextUrl.pathname;
   const isLoggedIn = request.cookies.get("userEmail");
 
