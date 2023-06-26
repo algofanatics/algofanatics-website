c336b70f56e2a39ffb83862ab5061f0ea5d33e92
Samson Ajulor <samsonajulor@gmail.com>
2023-06-26 12:00:17 +0100

    moded the docker image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "react-query";
